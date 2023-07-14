76dc7d65710e3f28a140f1b3b05037f8ad7705a9
tjonker97 <tshepangjonker@gmail.com>
2023-07-14 12:47:02 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Possible combination of numbers'
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar('0' + i);
+
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
+
