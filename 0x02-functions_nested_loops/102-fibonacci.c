892230c47f572d34264552fe9ceee0387035bf43
tjonker97 <tshepangjonker@gmail.com>
2023-07-18 19:00:46 -0700

    14. Fibinacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description:  prints the first 50 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%ld", j);
+		}
+		else if (i == 1)
+		{
+			printf(", %ld", k);
+		}
+		else
+		{
+			k += j;
+			j = k - j;
+
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
