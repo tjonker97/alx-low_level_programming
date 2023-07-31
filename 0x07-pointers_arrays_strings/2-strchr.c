60cefb93a250ef2869a91ff9ed211624269bb8fd
tjonker97 <tshepangjonker@gmail.com>
2023-07-31 08:16:11 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: The string to be searched
+ * @c: The character to be located
+ *
+ * Return: If c is found - a pointer to the first occurence
+ * if c is not found - NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+
+	}
+
+	return ('\0');
+}
