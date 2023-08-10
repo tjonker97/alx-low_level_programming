db94cf5781c864a97b324e144a06b343cf77df40
tjonker97 <tshepangjonker@gmail.com>
2023-08-10 16:08:40 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  * @s2: String to concatenate form
  * @n: number of bytes from s2 to concatenate to s1
  *
- * return: pointer to the resulting string
+ * Return: pointer to the resulting string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
