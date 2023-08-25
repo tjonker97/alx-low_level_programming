7c242665229338bda9497329bcedda718ccab4fb
tjonker97 <tshepangjonker@gmail.com>
2023-08-25 08:03:41 -0700

    0. Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,5 +18,3 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif
